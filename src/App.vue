e0b6dfafee0bccbc7dd02bdb54ee20d2605d5025
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-09-05 13:14:24 +0000

    Tela de usuários

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Segundou!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -18,6 +19,8 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+
+
 </template>
 
 <style scoped>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ nome }}, hoje é Terça-Feira!</h1>
+    <input type="number" v-model="contador"/>
+    <button @click="incrementar">Incrementar</button>
+    <input type="text" v-model="nome"/>
+    <p v-if="nome.length > 5">Texto longo!</p>
+    <p v-else>Texto curto!</p>
+    <button @click="incluir">Incluir</button>
+    <table>
+      <thead>
+        <td>Id</td>
+        <td>Nome</td>
+      </thead>
+      <tbody>
+        <tr v-for="usuario in usuarios" :key="usuario.id">
+          <td>{{ usuario.id }}</td>
+          <td>{{ usuario.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const nome = ref("Nome");
+  const usuarios = ref([{id:1, nome:"Zé"}, {id:2, nome:"Paula"}]);
+  const contador = ref(1);
+
+  function incrementar() {
+    contador.value++;
+  }
+
+  function incluir() {
+    usuarios.value.push({id:contador.value, nome: nome.value});
+  }
+</script>
 
 <style>
 @media (min-width: 1024px) {
